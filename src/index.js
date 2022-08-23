2b280bc36169e19e7e0bcd4181e663677fdf25e8
Felipe Pritsch Fahrion <felipefahrion@MacBook-Pro-de-Felipe.local>
2022-08-23 17:27:28 -0300

    add conversor logic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("ola")
+var REM_VALUE = 0.063
+
+var pxInputField = document.querySelector("#px-input")
+var remInputField = document.querySelector("#rem-input")
+
+pxInputField.addEventListener('change', (event) => {
+    const pxValue = event.target.value
+    remInputField.value = pxValue * REM_VALUE
+});
+
+remInputField.addEventListener('change', (event) => {
+    const remValue = event.target.value
+    pxInputField.value = remValue / REM_VALUE
+});
