fc8f9cb868d4b18692152777b7d0a1f5cfe72c5c
Felipe Pritsch Fahrion <felipefahrion@MacBook-Pro-de-Felipe.local>
2022-08-23 17:38:38 -0300

    refactor to extract to functions

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ var REM_VALUE = 0.063
 var pxInputField = document.querySelector("#px-input")
 var remInputField = document.querySelector("#rem-input")
 
+function remConversor(pxInput) {
+    return pxInput * REM_VALUE
+}
+
+function pxConversor(remInput) {
+    return remInput / REM_VALUE
+}
+
 pxInputField.addEventListener('input', (event) => {
     const pxValue = event.target.value
-    remInputField.value = pxValue * REM_VALUE
+    remInputField.value = remConversor(pxValue)
 });
 
 remInputField.addEventListener('input', (event) => {
     const remValue = event.target.value
-    pxInputField.value = remValue / REM_VALUE
+    pxInputField.value = pxConversor(remValue)
 });
