92ee75bc6411b7df653c599e67f817d2e6ae55fe
Felipe Pritsch Fahrion <felipefahrion@MacBook-Pro-de-Felipe.local>
2022-08-23 17:29:15 -0300

    change event trigger

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ var REM_VALUE = 0.063
 var pxInputField = document.querySelector("#px-input")
 var remInputField = document.querySelector("#rem-input")
 
-pxInputField.addEventListener('change', (event) => {
+pxInputField.addEventListener('input', (event) => {
     const pxValue = event.target.value
     remInputField.value = pxValue * REM_VALUE
 });
 
-remInputField.addEventListener('change', (event) => {
+remInputField.addEventListener('input', (event) => {
     const remValue = event.target.value
     pxInputField.value = remValue / REM_VALUE
 });
